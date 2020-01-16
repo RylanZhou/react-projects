871d57146c3309fca33dbb5bdfd729d66d162ae7
RylanZhou <43603673+RylanZhou@users.noreply.github.com>
2020-01-16 16:51:11 +0800

    Delete Chat App due to the problem of not being able to get messages.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-// import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 // import Quiz from './Quiz Page/index'
-// import BeachResort from './Beach Resort Page/index'
-// import { RoomProvider } from './Beach Resort Page/Context'
-import ChatApp from './Chat Page/index'
+import BeachResort from './Beach Resort Page/index'
+import { RoomProvider } from './Beach Resort Page/Context'
 
 // function App() {
 //   return (
@@ -12,19 +11,13 @@ import ChatApp from './Chat Page/index'
 //   )
 // }
 
-// function App() {
-//   return (
-//     <RoomProvider value="hello">
-//       <Router>
-//         <BeachResort />
-//       </Router>
-//     </RoomProvider>
-//   )
-// }
-
 function App() {
   return (
-    <ChatApp />
+    <RoomProvider value="hello">
+      <Router>
+        <BeachResort />
+      </Router>
+    </RoomProvider>
   )
 }
 
